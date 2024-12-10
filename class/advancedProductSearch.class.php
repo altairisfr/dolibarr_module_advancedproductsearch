5c7470f8a605171f471b5cdf80d3537079f25622
Christophe Battarel <christophe@altairis.fr>
2024-12-10 17:29:27 +0100

    add new hook adpsSearchComplete

--- class/advancedProductSearch.class.php
+++ class/advancedProductSearch.class.php
@@ -890,6 +890,9 @@ class AdvancedProductSearch
 		$output.= '</table>';
 		$output.= '</form>';
 
+		$hookmanager->executeHooks('adpsSearchComplete', $hookParam, $object, $action); // Note that $action and $object may have been modified by hook
+		$output .= $hookmanager->resPrint;
+
 		return $output;
 	}
 
